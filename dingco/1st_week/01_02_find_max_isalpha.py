ac306a433964d8b9e9d53cc95d671a9180dad5d4
YoulimKim <kimyoulim1016@gmail.com>
2024-12-23 19:19:31 +0900

    시간 복잡도, 공간 복잡도

--- dingco/1st_week/01_02_find_max_isalpha.py
+++ dingco/1st_week/01_02_find_max_isalpha.py
@@ -6,13 +6,22 @@
 # print(chr(97))
 
 # 다음과 같은 문자열을 입력받았을 때, 어떤 알파벳이 가장 많이 포함되어 있는지 반환하시오
+
 def find_max_occurred_alphabet(string):
+  ascii_num = 97
+  alphabet_occurrence_array = [0] * 26
+
   # 이 부분을 채워보세요!
-  alphabet_occurrence_array  = [0] * 26
-  return "a"
+  for char in string:
+    if char.isalpha():
+      index = ord(char) - ascii_num
+      alphabet_occurrence_array[index] += 1
+
+  return chr(alphabet_occurrence_array.index(max(alphabet_occurrence_array)) + ascii_num)
 
 
 result = find_max_occurred_alphabet
+
 print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
 print("정답 = e 현재 풀이 값 =", result("we love algorithm"))
 print("정답 = b 현재 풀이 값 =", result("best of best youtube"))
